1e9d192ad8235c8a2b1217efb335aaeca369961b
chrisslupi <gudmei@web.de>
2018-01-09 13:05:53 +0100

    Comment : first commit on Tue Jan  9 13:05:53 CET 2018

---
+++ installation/050-install-budgie-core-v1.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+
+##################################################################################################################
+
+
+# gnome
+
+echo "################################################################"
+echo "gnome and gdm"
+echo "################################################################"
+
+package="gnome"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+
+	fi
+
+
+fi
+
+
+package="gdm"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+
+	fi
+
+
+fi
+
+
+
+
+echo "################################################################"
+echo "budgie-desktop"
+echo "################################################################"
+
+package="budgie-desktop"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	fi
+
+fi
+
+
+
+
+
+
+echo "Display manager being activated"
+
+sudo systemctl enable gdm.service
+
+echo "Reboot and select the proper desktop environment"
+echo "with the gauge symbol."
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
+
+echo "Type sudo reboot"
---
+++ installation/110-install-printers-v1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+
+##################################################################################################################
+
+
+
+sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer --noconfirm --needed
+systemctl enable org.cups.cupsd.service
+systemctl start org.cups.cupsd.service
+
+
+echo "################################################################"
+echo "#########   printer management software installed     ##########"
+echo "################################################################"
